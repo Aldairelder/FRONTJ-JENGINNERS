d5e3053858ff763999785940317f3f27259b693d
Daniel <sd3danii@gmail.com>
2024-10-30 10:18:19 -0500

    commitlogin

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/home/home.vue';
 import Products from '../views/home/products.vue'; // Asegúrate de que la ruta del componente sea correcta
 import cart from '../views/home/shoopcart.vue';
+import login from '../views/home/login.vue'
 
 // Definir las rutas
 const routes = [
@@ -22,6 +23,11 @@ const routes = [
     path: '/carrito', // Ruta para la página de inicio
     name: 'cart',
     component: cart
+  },
+  {
+    path: '/Login', // Ruta para la página de inicio
+    name: 'login',
+    component: login
   }
 ];
 
---
+++ src/views/home/login.vue
@@ -0,0 +1,20 @@
+<script>
+import Inicio from '../../components/sections/inicio.vue';
+
+export default {
+  name: 'Login',
+  components: {
+    Inicio
+  }
+};
+</script>
+
+<template>
+  <div>
+    <Inicio />
+  </div>
+</template>
+
+<style>
+/* Aquí puedes agregar estilos personalizados para tu componente Login */
+</style>
